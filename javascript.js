f42fb2310f723b0363343a123b61abef5566b544
Nisha Bukke <sononenisha827@gmail.com>
2025-11-20 13:39:21 +0530

    mep changes

--- javascript.js
+++ javascript.js
@@ -75,19 +75,9 @@ var swiper = new Swiper(".mySwiper", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-    breakpoints: {
-      450: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 1,
-      },
-      1024: {
-        slidesPerView: 1,
-      },
-    },
+      }
   });
+
 
 
 
